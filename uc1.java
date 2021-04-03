41982991ea33f4e553a04696d7ddede4a5491279
Ashok Mane <ashokmane81800@gmail.com>
2021-04-03 13:31:39 +0530

    [ashok] created a file to check present or abscent employee

---
+++ uc1.java
@@ -0,0 +1,17 @@
+
+public class uc1 {
+	public static void main(String[] args) {
+		int min=0;
+		int max=1;
+		int a=(int)(Math.random()*(max-min+1)+min);
+		
+		
+		if(a==1){
+			System.out.println("Employee is present ");
+		}
+		else{
+			System.out.println("Employee is abscent");
+		}
+	}
+
+}
